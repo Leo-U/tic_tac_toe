5f0617e65129e75d9cae904d1d1ba20c4db484e1
Joel Howard <joelhoward2@gmail.com>
2022-11-11 01:57:16 -0500

    move game_won? to Board class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class Board
   attr_accessor :top, :middle, :bottom, :game_status
-  attr_reader :eye, :bird, :vertical_left, :vertical_middle, :vertical_right, :diagonal_forward, :diagonal_back, :total, :space
+  attr_reader :eye, :bird, :space, :vertical_left, :vertical_middle, :vertical_right, :diagonal_forward, :diagonal_back, :total
   def initialize
     @edge = "⣿
     ⣿
@@ -59,6 +59,17 @@ class Board
     @diagonal_forward = [@bottom[0], @middle[1], @top[2]]
     @diagonal_back = [@top[0], @middle[1], @bottom[2]]
     @total = [@top, @middle, @bottom]
+  end
+
+  def game_won?
+    self.top.uniq.length == 1 && !self.top.include?(self.space)\
+    || self.middle.uniq.length == 1 && !self.middle.include?(self.space)\
+    || self.bottom.uniq.length == 1 && !self.bottom.include?(self.space)\
+    || self.vertical_left.uniq.length == 1 && !self.vertical_left.include?(self.space)\
+    || self.vertical_middle.uniq.length == 1 && !self.vertical_middle.include?(self.space)\
+    || self.vertical_right.uniq.length == 1 && !self.vertical_right.include?(self.space)\
+    || self.diagonal_forward.uniq.length == 1 && !self.diagonal_forward.include?(self.space)\
+    || self.diagonal_back.uniq.length == 1 && !self.diagonal_back.include?(self.space) 
   end
 
   def print_board_row (row, edge = @edge)
@@ -139,22 +150,11 @@ until the_board.game_status == "victory" || the_board.game_status == "draw" do
 
   the_board.assign_data_for_conditionals
 
-  def game_won?(the_board)
-    the_board.top.uniq.length == 1 && !the_board.top.include?(the_board.space)\
-    || the_board.middle.uniq.length == 1 && !the_board.middle.include?(the_board.space)\
-    || the_board.bottom.uniq.length == 1 && !the_board.bottom.include?(the_board.space)\
-    || the_board.vertical_left.uniq.length == 1 && !the_board.vertical_left.include?(the_board.space)\
-    || the_board.vertical_middle.uniq.length == 1 && !the_board.vertical_middle.include?(the_board.space)\
-    || the_board.vertical_right.uniq.length == 1 && !the_board.vertical_right.include?(the_board.space)\
-    || the_board.diagonal_forward.uniq.length == 1 && !the_board.diagonal_forward.include?(the_board.space)\
-    || the_board.diagonal_back.uniq.length == 1 && !the_board.diagonal_back.include?(the_board.space) 
-  end
-
-  if game_won?(the_board) then 
+  if the_board.game_won? then 
     puts "Player #{player_switch[0]} wins! Game over!"
     the_board.game_status = "victory"
-  elsif !the_board.total.flatten.include?(the_board.space) && !game_won?(the_board)
-    puts "It's a draw."
+  elsif !the_board.total.flatten.include?(the_board.space) && !the_board.game_won?
+    puts "It's a draw. Game over."
     the_board.game_status = "draw"
   end
 
